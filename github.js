df8fe17143b8a001ffb17b040d7fd60be7b23c1c
Opilane TTHK <opilane@tthk.ee>
2019-11-11 15:52:24 +0200

    first commit

---
+++ github.js
@@ -0,0 +1,17 @@
+class Github{
+    constructor(){
+        this.client_id = '0bc6eb0431a9c37be91c';
+        this.client_secret='c53174ef3524cc1a862d7aeb0653bbec41e07222';
+        this.repos_count= 5;
+        this.repos_sort='created: asc';
+        this.user='martinlappolainen';
+    }
+    //get a user info
+
+    async getUserData(){
+        let urlProfile = `https://api.github.com/${this.user}?client_id=${this.client_id}&client_secret=${this.client_secret}`;
+        let urlRepos= `https:api.github.com/${this.user}/repos?per_page${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`;
+        const profileResponse = await fetch(urlProfile);
+
+    }
+}
